9400a93653a529a559c46b5f15feba3fe03e199a
nedagosa-cod <nestorcode4004@gmail.com>
2023-04-08 10:42:36 -0500

    login
    create login

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/styles/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <section class="bx-login">
+        <figure class="bx-login__figure">
+            <img src="./public/img/user.png" alt="user">
+        </figure>
+        <h1 class="bx-login__bienvenido">Bienvenido</h1>
+        <input placeholder="Usuario" class="bx-login__input" name="usuario" type="text">
+        <input placeholder="Contraseña" class="bx-login__input" name="clave" type="password   ">
+        <a href="#" class="bx-login__passfix">Olvidé Contraseña</a>
+        <button type="submit" class="bx-login__btn-login">INICIAR SESION</button>
+        <a href="#" class="bx-login__register">Registrarse</a>
+    </section>
+</body>
+</html>
